2a9d1caf0031142699e65a0a4d287661e779d174
Erkka Lehtoranta <erkka.lehtoranta@gmail.com>
2023-07-02 14:58:42 +0300

    Match-cased sanakirja3 if-tower

--- 7_dict_and_set/sanakirja3.py
+++ 7_dict_and_set/sanakirja3.py
@@ -24,63 +24,65 @@ def main():
     print_contents(english_spanish)
 
     while True:
-        command = input("[W]ord/[A]dd/[R]emove/[P]rint/[T]ranslate/[Q]uit: ")
+        command = input("[W]ord/[A]dd/[R]emove/[P]rint/[T]ranslate/[Q]uit: ").upper()
+        command = command.strip().upper()
 
-        if command == "W":
-            word = input("Enter the word to be translated: ")
-            if word in english_spanish:
-                print(f'{word} in Spanish is {english_spanish[word]}')
-            else:
-                print("The word", word, "could not be found from the dictionary.")
-
-        elif command == "A":
-            key = input("Give the word to be added in English: ")
-            value = input("Give the word to be added in Spanish: ")
-            if key and value:
-                english_spanish.update({key: value})
-            print_contents(english_spanish)
-
-        elif command == "R":
-            key = input("Give the word to be removed: ")
-            if key in english_spanish:
-                del english_spanish[key]
-            else:
-                print(f'The word {key} could not be found from the dictionary.')
-
-        elif command == "P":
-            values: list = sorted(english_spanish.items())
-            print("\nEnglish-Spanish")
-            for value in values:
-                print(value[0], value[1])
-
-            spanish_english = {key:value for (value,key) in english_spanish.items()}
-            values: list = sorted(spanish_english.items())
-            print("\nSpanish-English")
-            for value in values:
-                print(value[0], value[1])
-            print()
-
-        elif command == "T":
-            sentence = input(f'Enter the text to be translated into Spanish: ')
-            sentence_as_list: list = sentence.split()
-            translated = ""
-
-            for word in sentence_as_list:
+        match command:
+            case "W":
+                word = input("Enter the word to be translated: ")
                 if word in english_spanish:
-                    translated += english_spanish[word]
+                    print(f'{word} in Spanish is {english_spanish[word]}')
                 else:
-                    translated += word
-                translated += " "
+                    print("The word", word, "could not be found from the dictionary.")
 
-            print("The text, translated by the dictionary:")
-            print(translated.rstrip())
+            case "A":
+                key = input("Give the word to be added in English: ")
+                value = input("Give the word to be added in Spanish: ")
+                if key and value:
+                    english_spanish.update({key: value})
+                print_contents(english_spanish)
 
-        elif command == "Q":
-            print("Adios!")
-            return
+            case "R":
+                key = input("Give the word to be removed: ")
+                if key in english_spanish:
+                    del english_spanish[key]
+                else:
+                    print(f'The word {key} could not be found from the dictionary.')
 
-        else:
-            print("Unknown command, enter W, A, R, P, T or Q!")
+            case "P":
+                values: list = sorted(english_spanish.items())
+                print("\nEnglish-Spanish")
+                for value in values:
+                    print(value[0], value[1])
+
+                spanish_english = {key:value for (value,key) in english_spanish.items()}
+                values: list = sorted(spanish_english.items())
+                print("\nSpanish-English")
+                for value in values:
+                    print(value[0], value[1])
+                print()
+
+            case "T":
+                sentence = input(f'Enter the text to be translated into Spanish: ')
+                sentence_as_list: list = sentence.split()
+                translated = ""
+
+                for word in sentence_as_list:
+                    if word in english_spanish:
+                        translated += english_spanish[word]
+                    else:
+                        translated += word
+                    translated += " "
+
+                print("The text, translated by the dictionary:")
+                print(translated.rstrip())
+
+            case "Q":
+                print("Adios!")
+                return
+
+            case _:
+                print("Unknown command, enter W, A, R, P, T or Q!")
 
 if __name__ == "__main__":
     main()
