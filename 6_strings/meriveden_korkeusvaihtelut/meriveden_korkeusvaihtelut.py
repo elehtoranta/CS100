b6f191f502e3f255b7d0b939fb1daff540db7ff4
Erkka Lehtoranta <erkka.lehtoranta@gmail.com>
2023-07-31 04:57:21 +0300

    Beautified and returned 6.2: sealevels

--- 6_strings/meriveden_korkeusvaihtelut/meriveden_korkeusvaihtelut.py
+++ 6_strings/meriveden_korkeusvaihtelut/meriveden_korkeusvaihtelut.py
@@ -1,14 +1,17 @@
 """
-COMP.CS.100: Meriveden korkeusvaihtelut
-Tekijä: Erkka Lehtoranta
-Opiskelijanumero: ***REMOVED***
-Analyzing the development of sea level through some data markers.
+COMP.CS.100: Sea level measurements
+Author: Erkka Lehtoranta
+Student No: ***REMOVED***
+
+Description:
+    Analyzing the development of sea level through some data markers.
 """
 
 
 def get_median(sea_levels: list, levels_len: int) -> float:
     """
     Finds the median of a given list 'sea_levels'.
+
     :param sea_levels: list, of which the median is searched.
     :param levels_len: int, number of elements in the list sea_levels.
     :return: float, the median value of 'sea_levels'.
@@ -29,6 +32,7 @@ def get_median(sea_levels: list, levels_len: int) -> float:
 def get_mean(sea_levels: list, levels_len: int) -> float:
     """
     Calculates the mean value of given list (of floats), 'sea_levels'.
+
     :param sea_levels: list, values to average together
     :param levels_len: int, number of elements in the list sea_levels.
     :return: float, the mean value of a given list of floats 'sea_levels'
@@ -39,6 +43,7 @@ def get_mean(sea_levels: list, levels_len: int) -> float:
 def get_variance(sea_levels: list, mean: float, levels_len: int) -> float:
     """
     Calculates the variance of a given set of values.
+
     :param sea_levels: list, containing (sorted) sea level data as floats.
     :param mean: float, the mean value of the sea_levels list.
     :param levels_len: int, number of elements in the list sea_levels.
@@ -54,6 +59,7 @@ def get_variance(sea_levels: list, mean: float, levels_len: int) -> float:
 def get_deviation(variance: float) -> float:
     """
     Calculates the standard deviation for a given set of values.
+
     :param variance: float, precalculated variance of the set.
     :return: float, standard deviation of a given set.
     """
@@ -64,6 +70,7 @@ def get_levels() -> list:
     """
     Reads and saves the user input of sea level data. Input is read until empty
     input is given.
+
     :return: list, contains the raw sea level data.
     """
     sea_levels: list = []
@@ -86,13 +93,10 @@ def main():
     in any way, and the former will cause the program to crash. I considered
     extensive error handling to be out of the scope of this project (and out
     of scope of my Python skills).
-    :return: None
     """
     print("Enter seawater levels in centimeters one per line.\n"
           "End by entering an empty line.")
     sea_levels: list = get_levels()
-    # To avoid calculating the same list length multiple times, it's saved and
-    # passed as an argument later. This speeds up handling of large datasets.
     levels_len: int = len(sea_levels)
 
     if levels_len < 2:
